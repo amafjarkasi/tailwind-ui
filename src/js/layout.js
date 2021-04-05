91ae3e54fbd664c8f4ccee258e56e0c1e3fcca5a
amafjarkasi <amafjarkasi@gmail.com>
2021-04-05 08:53:17 +0000

    latest updates

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
